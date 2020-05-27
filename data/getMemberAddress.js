880b4b2f0de786fe4af843afc96f2c60e0e916f5
B Ruml <b@ruml.com>
2020-05-27 16:43:55 -0400

    adding module

---
+++ data/getMemberAddress.js
@@ -0,0 +1,22 @@
+/**
+ *  getMemberAddress.js - a module for the scraping program
+ *  May 26
+ */
+
+const getMemberAddress = function (addStr) {
+  console.log('==> addStr: ', addStr)
+
+  let memberAddress = {
+    street: "somewhere",
+    city: "nowhere"
+  }
+
+  // zip;
+  const zipre = /\d+-?\d\d\d\d$/
+
+
+
+  return memberAddress
+}
+
+ module.exports = getMemberAddress
